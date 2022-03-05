ecb07469d7b6e491a4ec23b17082c0647eeafccb
jacobprajan <jjacobprajan@gmail.com>
2022-03-05 15:03:54 +0000

    Two sum

---
+++ src/main/java/com/dev/TwoSum.java
@@ -0,0 +1,23 @@
+package com.dev;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int index = 0;
+        for(int n : nums) {
+            int otherNum = target - n;
+            if(map.containsKey(otherNum)) {
+                return new int[]{map.get(otherNum), index};
+            }
+            map.put(n, index);
+            ++index;
+        }
+        return new int[]{-1, -1};
+    }
+
+}
---
+++ src/test/java/com/dev/TwoSumTest.java
@@ -0,0 +1,74 @@
+package com.dev;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+    TwoSum twoSum;
+
+    @BeforeEach
+    void setUp() {
+        twoSum = new TwoSum();
+    }
+
+    @Test
+    @DisplayName("2, 7, 11, 15 and target 9")
+    void testSum() {
+        int nums[] = {2, 7, 11, 15};
+        int target = 9;
+        int expected[] = {0, 1};
+        assertArrayEquals(expected, twoSum.twoSum(nums, target), "Should return 0 and 1");
+    }
+
+    @Test
+    @DisplayName("3,2,4 and target 6")
+    void testSum2() {
+        int nums[] = {3, 2, 4};
+        int target = 6;
+        int expected[] = {1, 2};
+        assertArrayEquals(expected, twoSum.twoSum(nums, target), "Should return 1 and 2");
+    }
+
+    @Test
+    @DisplayName("3,3 and target 6")
+    void testSum3() {
+        int nums[] = {3, 3};
+        int target = 6;
+        int expected[] = {0, 1};
+        assertArrayEquals(expected, twoSum.twoSum(nums, target), "Should return 0 and 1");
+    }
+
+    @Test
+    @DisplayName("-1,3,2 and target 1")
+    void testSum4() {
+        int nums[] = {-1, 3, 2};
+        int target = 1;
+        int expected[] = {0, 2};
+        assertArrayEquals(expected, twoSum.twoSum(nums, target), "Should return 0 and 2");
+    }
+
+    @Test
+    @DisplayName("-1, 3, 2 and target -1")
+    void testSum5() {
+        int nums[] = {-1, 3, 2};
+        int target = 1;
+        int expected[] = {0, 2};
+        assertArrayEquals(expected, twoSum.twoSum(nums, target), "Should return 0 and 2");
+    }
+
+    @Test
+    @DisplayName("1,3,-2 and target 5")
+    void testSum6() {
+        int nums[] = {1, 3, -2};
+        int target = 5;
+        int expected[] = {-1, -1};
+        assertArrayEquals(expected, twoSum.twoSum(nums, target), "Should return -1 and -1");
+    }
+
+
+}
+
